6b463e01c4e74a1af87efad11779d077c8b070d9
Angelinahaoud077 <email_for_github_account@gmail.com>
2023-07-20 15:50:04 -0400

    Angelina Read In

--- Angelina/AngelinaCode.R
+++ Angelina/AngelinaCode.R
@@ -1,3 +1,8 @@
 library(readr)
-USvideos_USvideos <- read_csv("~/Desktop/USvideos - USvideos.csv")
+USvideos_USvideos <- read_csv("Angelina/videos/USvideos - USvideos.csv")
 View(USvideos_USvideos)
+
+install.packages("rjson")
+library("rjson")
+my_category_data <- fromJSON(file="data/US_category_id.json")
+View(my_category_data)
