3968a4fb27f68a16b61b1d2d8eba2678a50417f1
Raphael Mendones <lalaraffym@gmail.com>
2023-07-20 13:20:04 -0400

    Finished Cleaning Dataset of USVideos

--- RaphaelM/R.Scripts/CleaningData.R
+++ RaphaelM/R.Scripts/CleaningData.R
@@ -1,5 +1,154 @@
 data <- readRDS("data/USvideos.RDS")
-View(data)
+View(data) # trending_date = year/day/month
+
+# Libraries
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(janitor)
+
+# Renaming Columns
+renamed_data <- rename(data,
+                   channel = channel_title,
+                   videos_removed = video_error_or_removed)
+View(renamed_data)
+
+
+# Videos ranked by Views
+VidView <- renamed_data |>
+  arrange(views) |>
+  select(title, channel, likes, dislikes, comment_count)
+View(VidView)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
