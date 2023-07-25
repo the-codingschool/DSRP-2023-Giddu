0a880a8504b17c027249bc936e33e4ab8cc98fa3
Angelinahaoud077 <email_for_github_account@gmail.com>
2023-07-25 13:04:38 -0400

    plot

--- Angelina/AngelinaCode.R
+++ Angelina/AngelinaCode.R
@@ -6,3 +6,18 @@ install.packages("rjson")
 library("rjson")
 my_category_data <- fromJSON(file="data/US_category_id.json")
 View(my_category_data)
+
+install.packages(“lubridate”)
+library(lubridate)
+
+
+
+library(ggplot2)
+str(USvideos_USvideos)
+
+ggplot(data = USvideos_USvideos, aes(x=views, y=hour(publish_time))) +
+  geom_point ()+
+  labs(x= "Views",
+       y= "Release Time",
+       title= "Trending Youtube Videos Release Time VS. its correlating views(popularity) "
+  )
